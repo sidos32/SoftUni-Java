bc89f4dbda51a0d61df3b6596876b886ccde23fb
sidos32 <39670027+sidos32@users.noreply.github.com>
2020-02-15 18:01:31 +0200

    Create BombNumbers.java

---
+++ List Exercise/BombNumbers.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+ 
+public class Main {
+
+ private static void printList(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            System.out.print(number + " ");
+        }
+        
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        List<Integer> numbers = readIntList(scan);
+        String[] tokens = scan.nextLine().split("\\s+");
+        int number = Integer.parseInt(tokens[0]);
+        int power = Integer.parseInt(tokens[1]);
+ 
+        while (numbers.contains(number)) {
+            int bombIndex = numbers.indexOf(number);
+ 
+            int leftBound = Math.max(bombIndex - power, 0);
+            int rightBound = Math.min(bombIndex + power, numbers.size() - 1);
+ 
+            for (int i = rightBound; i >= leftBound; i--) {
+                numbers.remove(i);
+            }
+        }
+        int sum = 0;
+        for (Integer integer : numbers) {
+            sum += integer;
+        }
+        System.out.println(sum);
+    }
+ 
+    private static boolean isValidIndex(int index, List<Integer> numbers) {
+        return index >= 0 && index <= numbers.size();
+    }
+ 
+    private static List<Integer> readIntList(Scanner scan) {
+        return Arrays
+                .stream(scan.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+ 
+   
+    }
+}
