c766d74da589f8b19ef50cd5d815f05abd94b0e2
sidos32 <39670027+sidos32@users.noreply.github.com>
2020-02-21 21:51:18 +0200

    OrderByAge

---
+++ Object&Classes Exercise/OrderByAge/Main.java
@@ -0,0 +1,48 @@
+package SoftUni.OrderByAge;
+
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<Persons> peoples = new ArrayList<Persons>();
+
+        boolean isEndOfProgram = false;
+
+        while (!isEndOfProgram) {
+            String[] tokens = sc.nextLine().split(" ");
+                int i = tokens.length;
+
+            if (tokens[0].equals("End")) {
+                isEndOfProgram = true;
+            }else{
+
+                String name = tokens[0];
+                String ID = tokens[1];
+                int age = Integer.parseInt(tokens[2]);
+
+                Persons person = new Persons(name, ID, age);
+
+                peoples.add(person);
+            }
+        }
+
+        Collections.sort(peoples, new Comparator<Persons>() {
+            @Override
+            public int compare(Persons p1, Persons p2) {
+                return p1.getAge() - p2.getAge();
+            }
+        });
+
+        for (Persons it:peoples) {
+            System.out.println(it.toString());
+        }
+
+    }
+
+}
+
+
---
+++ Object&Classes Exercise/OrderByAge/Persons.java
@@ -0,0 +1,35 @@
+package SoftUni.OrderByAge;
+
+public class Persons {
+
+
+    public Persons(String name, String ID, int age) {
+        this.name = name;
+        this.ID = ID;
+        this.age = age;
+    }
+
+    private String name;
+    private String ID;
+    private int age;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("%s with ID: %s is %d years old.", name, ID, age);
+    }
+}
+
