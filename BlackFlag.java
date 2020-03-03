a9280f28e9003a632afd16c57584798c831d5082
sidos32 <39670027+sidos32@users.noreply.github.com>
2020-03-03 18:03:02 +0200

    Create BlackFlag.java

---
+++ BlackFlag.java
@@ -0,0 +1,35 @@
+package SoftUni;
+
+import java.util.Scanner;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int days = Integer.parseInt(sc.nextLine());
+        int dailyPlunder = Integer.parseInt(sc.nextLine());
+        double expectedPlunder = Double.parseDouble(sc.nextLine());
+        double totalPlunder = 0.0D;
+
+        for(int i = 1; i <= days; ++i) {
+            totalPlunder += (double)dailyPlunder;
+            if (i % 3 == 0) {
+                totalPlunder += (double)dailyPlunder * 0.5D;
+            }
+
+            if (i % 5 == 0) {
+                totalPlunder -= 0.3D * totalPlunder;
+            }
+        }
+
+        if (totalPlunder >= expectedPlunder) {
+            System.out.printf("Ahoy! %.2f plunder gained.", totalPlunder);
+        } else {
+            double result = totalPlunder / expectedPlunder * 100.0D;
+            System.out.printf("Collected only %.2f%% of the plunder.", result);
+        }
+
+    }
+}
