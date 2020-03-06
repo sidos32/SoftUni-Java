b3254aab80ed89381a20d7ae31a9e74ea05e8014
sidos32 <39670027+sidos32@users.noreply.github.com>
2020-03-06 14:58:14 +0200

    Update 1. CountCharsInAString.java

--- Maps, Lambda and Stream API/1. CountCharsInAString.java
+++ Maps, Lambda and Stream API/1. CountCharsInAString.java
@@ -8,30 +8,23 @@ public class CountCharsInAString {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
         String input = scanner.nextLine();
-        int inputSize = input.length();
-        Map<Character,Integer> mapOfStrings = new LinkedHashMap<>();
 
+        Map<Character, Integer> mapOfStrings = new LinkedHashMap<>();
 
-        for (int i = 0; i < inputSize; i++) {
-
-
+        for (int i = 0; i < input.length(); ++i) {
             char ch = input.charAt(i);
-            if(ch != 32){
+            if (ch != 32) {
 
-                if(!mapOfStrings.containsKey(ch)){
-                    mapOfStrings.put(ch,0); //Maybe 1 not 0
-                }
-                mapOfStrings.put(ch,mapOfStrings.get(ch)+1);
+                mapOfStrings.putIfAbsent(ch, 0);
+
+
+                mapOfStrings.put(ch, mapOfStrings.get(ch) + 1);
             }
 
         }
 
-
-        for (var entry : mapOfStrings.entrySet()) {
-            System.out.printf("%s -> %d%n",entry.getKey(),entry.getValue());
-        }
+        mapOfStrings.forEach((key, value) -> System.out.printf("%s -> %d%n", key, value));
 
 
     }
