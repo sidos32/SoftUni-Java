d5222b9496e43008775574bbd1cc16882e336bdf
sidos32 <39670027+sidos32@users.noreply.github.com>
2020-03-26 11:35:38 +0200

    Create 6. ExtractEmails.java

---
+++ Regular Expressions/Regular Expressions - Exercise/6. ExtractEmails.java
@@ -0,0 +1,27 @@
+package SoftUni.RegularExpressions;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractEmails {
+    public static void main(String[] args) {
+        Scanner scanner =  new Scanner(System.in);
+
+        String emailRegex = "(?:[a-z0-9!#$%&'*+=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+=?^_`{|}~-]+)*|\"" +
+                "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])" +
+                "*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])" +
+                "?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}" +
+                "(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:" +
+                "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])";
+
+        String inputText = scanner.nextLine();
+        Pattern emailPattern = Pattern.compile(emailRegex);
+        Matcher emailMatcher = emailPattern.matcher(inputText);
+
+        while (emailMatcher.find()){
+            System.out.println(emailMatcher.group());
+        }
+
+    }
+}
