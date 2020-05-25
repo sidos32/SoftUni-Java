dad99f2a8236c8d3fe781f42e44ef22502532310
sidos32 <39670027+sidos32@users.noreply.github.com>
2020-05-25 12:04:09 +0300

    Create MaximumElement.java

---
+++ Java-Advance/StacksAndQueues/MaximumElement.java
@@ -0,0 +1,29 @@
+package SoftUni.JavaAdvance.StacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class MaximumElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        ArrayDeque<Integer> numbers = new ArrayDeque<>();
+
+     /*   for (int i = 0; i < n; i++) {
+            String[] tokens = scanner.nextLine().split("\\s+");
+
+            switch (tokens[0]) {
+                case "1":
+                    numbers.push(Integer.parseInt(tokens[1]));
+                    break;
+                case "2":
+                    numbers.pop();
+                    break;
+                case "3":
+                    System.out.println(Collections.max(numbers));
+                    break;
+            }
+        }*/
+    }
+    }
